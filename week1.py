2b04531fcca782a09d2b6171424299a5ecfe2e79
John3974 <ocsjohnte@gmail.com>
2025-11-05 07:53:53 -0800

    Add files via upload

---
+++ week1.py
@@ -0,0 +1,17 @@
+# A word Game Program: "The unexpected visitor"
+# A suser for words
+adjectives = input("input an adjective: ")
+animals = input("input an animal: ")
+verb1 = input("input a verb: ")
+exclamation = input("input an exclamation: ").capitalize()
+verb2 = input("input another verb: ")
+verb3 = input("input one more verb: ")
+
+
+# The story
+story = f"""
+The night i could not belive what i saw .out of the blue, a {adjectives} {animals} satrted to {verb1}  across the room.
+"{exclamation}!" i yelled in shock. My fist instinct was to {verb2} as fast as i could, that actually made it {verb3} even-more faster! in front.StopIteration
+"""
+# Print the story
+print(story)
